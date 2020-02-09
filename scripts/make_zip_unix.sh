7affb12948c48cabcdb6d8ccbcdd88e84810e0d3
Stephen Sherratt <stephen@sherra.tt>
2020-02-09 14:39:07 +1100

    test

--- scripts/make_zip_unix.sh
+++ scripts/make_zip_unix.sh
@@ -6,12 +6,12 @@ echo $ZIP_NAME
 
 TMP=$(mktemp -d)
 mkdir $TMP/$ZIP_NAME
-cp target/$MODE/rip_graphical $TMP/$ZIP_NAME/rip_graphic
-cp target/$MODE/rip_ansi_terminal $TMP/$ZIP_NAME/rip_terminal
+cp target/$MODE/rip_graphical $TMP/$ZIP_NAME/rip-graphic
+cp target/$MODE/rip_ansi_terminal $TMP/$ZIP_NAME/rip-terminal
 if [ -f target/$MODE/rip_graphical_opengl ]; then
-  cp target/$MODE/rip_graphical_opengl $TMP/$ZIP_NAME/rip_graphic_opengl
+  cp target/$MODE/rip_graphical_opengl $TMP/$ZIP_NAME/rip-graphic-opengl
 fi
 pushd $TMP
-zip $ZIP_NAME.zip $ZIP_NAME/rip_graphic $ZIP_NAME/rip_terminal
+zip $ZIP_NAME.zip $ZIP_NAME/*
 popd
 mv $TMP/$ZIP_NAME.zip .
