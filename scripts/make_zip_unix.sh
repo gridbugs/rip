9b903a4706854fc2384444f811630108d306ae67
Stephen Sherratt <stephen@sherra.tt>
2020-11-22 23:57:07 +1100

    Remove opengl targets

--- scripts/make_zip_unix.sh
+++ scripts/make_zip_unix.sh
@@ -8,9 +8,6 @@ TMP=$(mktemp -d)
 mkdir $TMP/$ZIP_NAME
 cp -v target/$MODE/rip_graphical $TMP/$ZIP_NAME/rip-graphic
 cp -v target/$MODE/rip_ansi_terminal $TMP/$ZIP_NAME/rip-terminal
-if [ -f target/$MODE/rip_graphical_opengl ]; then
-  cp -v target/$MODE/rip_graphical_opengl $TMP/$ZIP_NAME/rip-graphic-opengl
-fi
 
 cp -v extras/unix/* $TMP/$ZIP_NAME
 
